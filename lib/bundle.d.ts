fac647269a1b82c7fbe86a10dd7451cb8898cf32
Alex Ware <alex.ware@guardian.co.uk>
2019-01-28 16:08:44 +0000

    removing compiled source

--- lib/bundle.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import archiver = require("archiver");
-import { Writable } from "stream";
-export declare const compressToStream: (path: string, method: archiver.Format, stream: Writable) => Promise<void>;
-export declare const getAllFiles: (path: string) => Promise<string[]>;
--- lib/upload.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { S3 } from "aws-sdk";
-import { Readable } from "stream";
-import { Action } from "./config";
-import { Manifest } from "./manifest";
-export declare const uploadStream: (name: string, stream: Readable, manifest: Manifest, action?: Action | undefined) => Promise<S3.ManagedUpload.SendData>;
-export declare const uploadAction: (manifest: Manifest, action: Action) => Promise<S3.ManagedUpload.SendData> | Promise<S3.ManagedUpload.SendData[]>;
