a87d3f750322e4f1cddf18ab9b6561608553ccf0
Alex Ware <alex.ware@guardian.co.uk>
2019-06-25 16:44:49 +0100

    oops

--- bin.js
+++ bin.js
@@ -1,10 +1,7 @@
 #! /usr/bin/env node
-const running = true
-const wait = () => { if (running) setTimeout(wait, 100) }
 const riffraff = require("./lib/index")
 riffraff.deploy().then(() => {
   console.log("Upload complete.")
-  running = false
 }).catch((error) => {
   console.error("Upload failed. ")
   console.error(error)
